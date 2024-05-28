a5b51149d80666e7e3fddcb8c9a4fdaf559a3585
Camdyn Hamilton <c02hamil@gmail.com>
2024-05-28 16:14:22 -0400

    Reorganize files and add js files.

---
+++ data/countries.js
@@ -0,0 +1,16 @@
+export class Country {
+  flag;
+  name;
+  capital;
+  continent;
+  population;
+  area;
+
+  constructor(countryDetails) {
+    this.flag = countryDetails.flag;
+    this.name = countryDetails.name;
+    
+  }
+
+  
+}
