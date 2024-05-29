5449284311fa8ca0179cb8d74b7080bd128b7c0b
Camdyn Hamilton <c02hamil@gmail.com>
2024-05-29 11:38:07 -0400

    Created welcome message for first time users

--- scripts/index.js
+++ scripts/index.js
@@ -40,3 +40,29 @@ function renderHTML() {
 }
 
 renderHTML();
+const noMessage = localStorage.getItem('noMessage');
+if (!noMessage) {
+  document.querySelector('.js-welcome-message').innerHTML = `<div class="faded-background">
+  <div class="welcome-box">
+    <div class="welcome-title">Welcome to Djibouti - the ultimate geography guessing game!</div>
+    <div>How to Play:</div>
+    <div>1. Enter a country name to start.</div>
+    <div>2. Receive hints based on:</div>
+    <ul>
+      <li>The starting letter of the country.</li>
+      <li>The starting letter of the capital.</li>
+      <li>The continent the country is located in.</li>
+      <li>Whether the population is higher or lower.</li>
+      <li>Whether the area is larger or smaller.</li>
+    </ul>
+    <div>3. Use the hints to make your next guess.</div>
+    <div>4. Continue guessing until you identify the correct country.</div>
+    <div>Test your knowledge, sharpen your geography skills, and see how quickly you can guess the right country. Good luck and have fun exploring the world with Djibouti.</div>
+    <div style="text-align: center";><button class="get-started-button js-get-started-button">Get Started</button></div>
+  </div>
+</div>`
+  document.querySelector('.js-get-started-button').addEventListener('click', () => {
+    document.querySelector('.faded-background').remove();
+    localStorage.setItem('noMessage', true);
+  })
+}
