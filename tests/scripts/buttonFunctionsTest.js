16dcf284334ec06e2ff6d4fd48a13034c099754b
Camdyn Hamilton <c02hamil@gmail.com>
2024-06-04 17:46:22 -0400

    Add test for reset button

--- tests/scripts/buttonFunctionsTest.js
+++ tests/scripts/buttonFunctionsTest.js
@@ -1,4 +1,4 @@
-import {userClear} from '../../scripts/buttonFunctions.js'
+import {userClear, userReset} from '../../scripts/buttonFunctions.js'
 
 describe('test suite: Button functions', () => {
   afterEach(() => {
@@ -9,11 +9,20 @@ describe('test suite: Button functions', () => {
     <div class="endgame js-endgame">
     </div>`;
     document.querySelector('.js-welcome-message').innerHTML = '';
-  })
+  });
 
   it('creates event listener for clear button', () => {
+    document.querySelector('.js-user-choice').innerHTML = '<button class="user-clear js-user-clear">Clear</buttton>';
     spyOn(document.querySelector('.js-user-clear'), 'addEventListener');
     userClear();
     expect(document.querySelector('.js-user-clear').addEventListener).toHaveBeenCalledTimes(1);
-  })
+  });
+
+  it('creates event listener for reset button', () => {
+    document.querySelector('.js-endgame').innerHTML = '<button class="reset-button js-reset-button">Reset</button></div>';
+    console.log(document.querySelector('.js-endgame'));
+    spyOn(document.querySelector('.js-reset-button'), 'addEventListener');
+    userReset();
+    expect(document.querySelector('.js-reset-button').addEventListener).toHaveBeenCalledTimes(1);
+  });
 });
